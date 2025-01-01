15ff3857b75d301df997dffa98d97969973d7347
Fernando Bizzotto <fbizzotto@blackpointcyber.com>
2025-01-01 14:10:29 +0000

    add hex_to_base_64 tests

--- src/bin/hex_to_base_64.rs
+++ src/bin/hex_to_base_64.rs
@@ -5,15 +5,29 @@ use cryptopals::string::{base64::base_64_encode_bytes, hex::hex_decode_bytes};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let hex = args[1].as_bytes();
+    let hex = &args[1];
 
-    let hex_bytes = hex_decode_bytes(hex).expect("cannot parse hex string");
-    for byte in &hex_bytes {
-        print!("{:08b}.", byte);
+    println!("{}", hex_to_base_64(hex));
+}
+
+fn hex_to_base_64(hex: &str) -> String {
+    let hex_bytes = hex_decode_bytes(hex.as_bytes()).unwrap();
+    let base_64_bytes = base_64_encode_bytes(&hex_bytes);
+    let str = String::from_utf8(base_64_bytes).unwrap();
+    return str;
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_convert() {
+        let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+        let result = hex_to_base_64(input);
+        assert_eq!(
+            result,
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+        );
     }
-    println!();
-
-    let output = base_64_encode_bytes(&hex_bytes);
-
-    print!("{}", std::str::from_utf8(&output).unwrap());
 }
